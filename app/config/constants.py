24c2a525851b65b25f9e8343e6cd150ad21cda87
ivanvmoreno <contacto@ivanmoreno.me>
2020-03-13 12:41:37 +0100

    [improvement] Restructured project constants

--- app/config/constants.py
+++ app/config/constants.py
@@ -1,30 +1,26 @@
-from typing import NamedTuple
 from os import getenv
 
 DB_URI = getenv('DB_URI')
 AMQP_URI = getenv('AMQP_URI')
-AMQP_QUEUES = {
-    'customers': {
-        'customers_credit_standing': 'customers_credit_standing',
-        'customers_customer_exist': 'customers_customer_exist',
-    },
-    'notifications': {
-        'notifications_email_order_approved': 'notifications_email_order_approved',
-        'notifications_email_order_cancelled': 'notifications_email_order_cancelled',
-    },
-    'orders': {
-        'orders_new_order': 'orders_new_order',
-        'orders_order_created': 'orders_order_created',
-        'orders_cancel_order': 'orders_cancel_order',
-        'orders_order_status': 'orders_order_status',
-    },
-    'products': {
-        'products_product_stock': 'products_product_stock',
-        'products_add_order_product': 'products_add_order_product',
-    },
+EVENTS_CUSTOMERS = {
+    'customers_credit_standing': 'customers_credit_standing',
+    'customers_customer_exist': 'customers_customer_exist',
 }
-EVENTS_CUSTOMERS = AMQP_QUEUES['customers']
-EVENTS_NOTIFICATIONS = AMQP_QUEUES['notifications']
-EVENTS_ORDERS = AMQP_QUEUES['orders']
-EVENTS_PRODUCTS = AMQP_QUEUES['products']
-TCP_PORT = 3000
+EVENTS_NOTIFICATIONS = {
+    'notifications_email_order_approved': 'notifications_email_order_approved',
+    'notifications_email_order_cancelled': 'notifications_email_order_cancelled',
+}
+EVENTS_ORDERS = {
+    'orders_new_order': 'orders_new_order',
+    'orders_order_created': 'orders_order_created',
+    'orders_cancel_order': 'orders_cancel_order',
+    'orders_order_status': 'orders_order_status',
+}
+EVENTS_PRODUCTS = {
+    'products_product_stock': 'products_product_stock',
+    'products_add_order_product': 'products_add_order_product',
+}
+TCP_PORT_CUSTOMERS = 8001
+TCP_PORT_NOTIFICATIONS = 8002
+TCP_PORT_ORDERS = 8003
+TCP_PORT_PRODUCTS = 8004
